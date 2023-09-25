4bde84a93b92285d96a6ef75cd595f01f7a0ff9b
Deniz Sevgiler <dsevgiler@hotmail.com>
2023-09-25 12:50:44 +0300

    test geliştirmesi.

--- Test/Controllers/WeatherForecastController.cs
+++ Test/Controllers/WeatherForecastController.cs
@@ -6,6 +6,7 @@ namespace Test.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
+        // test geliþtirmeleri 
         private static readonly string[] Summaries = new[]
         {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
