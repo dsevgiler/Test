ef0424d308187b9cba5e132482781c51a10bf10f
Deniz Sevgiler <dsevgiler@hotmail.com>
2023-10-18 16:41:22 +0300

    testde result eklendi.

---
+++ Test/Entity/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace Test.Entity
+{
+    public class Product
+    {
+        public int Id { get; set; }     
+        public string Name { get; set; }
+        public int Price { get; set; } 
+
+
+    }
+}
